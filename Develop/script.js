b773295368f72576a7f75fed15c4b66f1faa2c3f
aidabyte <ablinstrubyte@gmail.com>
2020-09-27 13:19:01 -0500

    wrote out prompts

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,20 @@ function generatePassword(){
   var numbers = ["1,2,3,4,5,6,7,8,9"];
   var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  var randomCharacters = "";
+  var randomCharacters = "#@!$%^&*()_-=+;:.?[]{}";
+  
+//prompts for code
+var passLength = prompt ("choose a number between 8 and 128 ")
+var lowerCase = confirm ("Do you want lowercase letters?")
+var upperCase = confirm ("Do you want lowercase letters?")
+var numbers = confirm ("Do you want numbers?")
+var specialChar = confirm ("Do you want special characters?")
+
+
+
+  
+
+
 
 }
 
