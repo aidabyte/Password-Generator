6a3d64ce8691f9a1fb7cb1665430ce057d69b6cb
aidabyte <ablinstrubyte@gmail.com>
2020-09-27 13:43:31 -0500

    if statement

--- Develop/script.js
+++ Develop/script.js
@@ -9,19 +9,25 @@ function writePassword() {
   passwordText.value = password;
 
 }
-//variables for the cuser to choose from
+//variables for the user to choose from
 function generatePassword(){
   var numbers = ["1,2,3,4,5,6,7,8,9"];
   var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   var randomCharacters = "#@!$%^&*()_-=+;:.?[]{}";
+  var passwordLength = "";
   
 //prompts for code
 var passLength = prompt ("choose a number between 8 and 128 ")
+  if (passLength>8) {
+    alert ("You're password isnt between 8 of 128 characters")
+  }
 var lowerCase = confirm ("Do you want lowercase letters?")
 var upperCase = confirm ("Do you want lowercase letters?")
 var numbers = confirm ("Do you want numbers?")
 var specialChar = confirm ("Do you want special characters?")
+
+
 
 
 
