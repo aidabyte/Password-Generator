c62e58c032319d1b3ca69ce4124f8066d65745e8
aidabyte <ablinstrubyte@gmail.com>
2020-09-27 19:34:26 -0500

    changed array to object because it kept breaking the code

--- Develop/script.js
+++ Develop/script.js
@@ -6,30 +6,31 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+
+// variables for the user to choose from
+  numbers = "0123456789",
+  lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  specialChar = "!#$%@&^*()"
+  passLength = ""
 }
-//variables for the user to choose from
-function generatePassword(){
-  var numbers = ["1,2,3,4,5,6,7,8,9"];
-  var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  var randomCharacters = "#@!$%^&*()_-=+;:.?[]{}";
-  var passwordLength = "";
-  
+
+
 //prompts for code
-var passLength = prompt ("choose a number between 8 and 128 ")
-  if (passLength>=8) {
-    alert ("You're password isnt between 8 of 128 characters")
+function generatePassword(){
+
+  var passLength = prompt("choose a number between 8 and 128");
+  if (passLength< 8 || passLength> 128) {
+    alert("You're password isnt between 8 of 128 characters, please try again");
     return;
   }
- 
-var lowerCase = confirm ("Do you want lowercase letters?")
+var lowerCase = confirm ("Do you want lowercase letters?");
+var upperCase = confirm ("Do you want lowercase letters?");
+var numbers = confirm ("Do you want numbers?");
+var specialChar = confirm ("Do you want special characters?");
 
 
-var upperCase = confirm ("Do you want lowercase letters?")
-var numbers = confirm ("Do you want numbers?")
-var specialChar = confirm ("Do you want special characters?")
 
 
 
