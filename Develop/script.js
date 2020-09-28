bec281198d113f3b44a04fbf0c3c37116fd55faa
aidabyte <ablinstrubyte@gmail.com>
2020-09-27 20:44:13 -0500

    added more variables

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,24 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
 
 
 // variables for the user to choose from
-  numbers = "0123456789",
-  lowerCase = "abcdefghijklmnopqrstuvwxyz"
-  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  specialChar = "!#$%@&^*()"
-  passLength = ""
-}
+  var numbers = ["1","2","3","4","5","6","7","8","9"];
+  var lowerCase= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var specialChar = ["!","@","#","$","%","&",];
+
+  var cLength = "";
+  var cSpecialCharacter;
+  var cNumberCharacter;
+  var cUpperCase;
+  var cLowerCase;
+  
 
 
 //prompts for code
@@ -29,6 +34,16 @@ var lowerCase = confirm ("Do you want lowercase letters?");
 var upperCase = confirm ("Do you want lowercase letters?");
 var numbers = confirm ("Do you want numbers?");
 var specialChar = confirm ("Do you want special characters?");
+
+//confirm 
+
+
+
+
+
+
+
+
 
 
 
